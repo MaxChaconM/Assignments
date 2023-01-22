64eab70b40e00588f6602f607020ed17dd655d12
mChaconRelish <mchacon@reli.sh>
2023-01-21 21:57:49 -0600

    ADD assignment files

---
+++ 01_tools/Test_print.bat
@@ -0,0 +1,16 @@
+a) Create the directory "shell_test"
+    md C:\Users\MaxCh\Desktop\Shell_Test
+
+b) Create the file "test_print.py" with a simple print into the directory
+
+c) Rename the file to "new_test_print.py"
+ren C:\Users\MaxCh\Desktop\Shell_Test\test_print.py new_test_print.py
+
+d) List what is in the directory "shell_test" including their file permissions
+dir C:\Users\MaxCh\Desktop\Shell_Test
+
+e) Execute the Python file and call the simple print
+python C:\Users\MaxCh\Desktop\Shell_Test\\new_test_print.py
+
+f) Remove the directory "shell_test" with its content
+del C:\Users\MaxCh\Desktop\Shell_Test
---
+++ 01_tools/test_print.py
@@ -0,0 +1,1 @@
+print ("Hello people from python advanced!")
